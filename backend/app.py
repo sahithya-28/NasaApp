83d5b3d6fa59c4fffa4b72204e95254c7d97a95d
Sahithya <tsahithya28@gmail.com>
2025-12-15 18:32:12 +0530

    update

--- backend/app.py
+++ backend/app.py
@@ -56,7 +56,7 @@ def get_weather():
 # --- Root Endpoint ---
 @app.route("/")
 def home():
-    return "Backend server is running!"
+    return "Backend server is running!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
